9a09e95f2cf13d45e71c7b70174aa64f5b662a1f
kchars <74715088+kchars@users.noreply.github.com>
2021-05-18 17:46:16 +0900

    YedamAndroid 0518 2

--- MyProject/app/src/main/java/com/example/myproject/ThirdActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/ThirdActivity.java
@@ -5,32 +5,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.myproject.R;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class ThirdActivity extends YouTubeBaseActivity {
-    //객체 선언
     YouTubePlayerView playerView;
     YouTubePlayer player;
 
-    //유튜브 API KEY와 동영상 ID 변수 설정
-    private static String API_KEY = "AIzaSyA9EcPeFzG6vV5Nc5cLL8AB_1MMeP1015E";
+    private static String API_KEY = "AIzaSyBWOOsRWLGOQE8hTgPcTpyCFeAHnYWsOYk";
     private static String videoId = "Ktazno7rgZg";
 
-    //logcat 사용 설정
-    private static final String TAG = "ThirdActivity";
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_third);
+
         initPlayer();
-        Button btnPlay = findViewById(R.id.youtubeBtn);
-        btnPlay.setOnClickListener(new View.OnClickListener() {
+
+        Button button = findViewById(R.id.button_play);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 playVideo();
@@ -38,49 +34,35 @@ public class ThirdActivity extends YouTubeBaseActivity {
         });
     }
 
-    private void playVideo() {
-        if (player != null) {
-            if (player.isPlaying()) {
-                player.pause();
-            }
-            player.cueVideo(videoId);
-        }
-    }
-
-    //유튜브 플레이어 메서드
-    private void initPlayer() {
-        playerView = findViewById(R.id.youTubePlayerView);
+    public void initPlayer(){
+        playerView = findViewById(R.id.playerView);
         playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 player = youTubePlayer;
+
                 player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
                     @Override
-                    public void onLoading() {
-                    }
+                    public void onLoading() {}
 
                     @Override
                     public void onLoaded(String id) {
-                        Log.d(TAG, "onLoaded: " + id);
+                        Log.d("PlayerView", "onLoaded 호출됨: " + id);
+
                         player.play();
                     }
 
                     @Override
-                    public void onAdStarted() {
-                    }
+                    public void onAdStarted() {}
 
                     @Override
-                    public void onVideoStarted() {
-                    }
+                    public void onVideoStarted() {}
 
                     @Override
-                    public void onVideoEnded() {
-                    }
+                    public void onVideoEnded() {}
 
                     @Override
-                    public void onError(YouTubePlayer.ErrorReason errorReason) {
-                        Log.d(TAG, "onError: " + errorReason);
-                    }
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {}
                 });
             }
 
@@ -89,5 +71,14 @@ public class ThirdActivity extends YouTubeBaseActivity {
             }
         });
     }
-}
 
+    public void playVideo(){
+        if(player != null){
+            if(player.isPlaying()){
+                player.pause();
+            }
+            player.cueVideo(videoId);
+        }
+    }
+
+}
